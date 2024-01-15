b7ac00eed87679c05bfabf63dda918a916d97bda
techie-kp <143493165+techie-kp@users.noreply.github.com>
2024-01-15 14:38:31 +0530

    Add files via upload

---
+++ ArachneDB/main.go
@@ -0,0 +1,15 @@
+package LibraDB
+
+func LibraDB() {
+	path := "libra.db"
+	db, _ := Open(path, DefaultOptions)
+
+	tx := db.WriteTx()
+	name := []byte("test")
+	collection, _ := tx.CreateCollection(name)
+
+	key, value := []byte("key1"), []byte("value1")
+	_ = collection.Put(key, value)
+
+	_ = tx.Commit()
+}
